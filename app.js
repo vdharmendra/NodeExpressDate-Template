f7936e1be4c9ce562b2679650d58d21726d16b18
vdharmendra <dhiru860.com>
2025-05-16 12:08:59 +0530

    UPDATING CHAPTER 3: SWITCH & RUNNING CODE INSIDE THE EJS TEMPLATE

--- app.js
+++ app.js
@@ -22,14 +22,42 @@ app.get("/", function(req, res){
 
 
     // ##### Chap: 2
-    console.log(currentday);
-    if(currentday == 6 || currentday == 0){
-        day = "weekendddd";
-    }else{
-        day = "weekdayyyy";
+    // console.log(currentday);
+    // if(currentday == 6 || currentday == 0){
+    //     day = "weekendddd";
+    // }else{
+    //     day = "weekdayyyy";
+    // }
+    // res.render("list", {kindofday: day});
+
+
+    // ###### CHAPTER 3
+    switch(currentday){
+        case 0:
+        day = "Sunday";
+        break;
+        case 1:
+        day = "Monday";
+        break;
+        case 2:
+        day = "Tuesday";
+        break;
+        case 3:
+        day = "Wedensday";
+        break;
+        case 4:
+        day = "Thursday";
+        break;
+        case 5:
+        day = "Friday";
+        break;
+        case 6:
+        day = "Saturday";
+        break;
+        default:
+            console.log("Error");
     }
     res.render("list", {kindofday: day});
-    
 });
 
 app.listen(3000, function(){
