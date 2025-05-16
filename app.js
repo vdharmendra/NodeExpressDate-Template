8b0520ec9ddfeeba7e291ebf930609a58e08f274
vdharmendra <dhiru860.com>
2025-05-16 12:49:04 +0530

    PASSING DATA FROM WEB PAGE TO SERVER USING BODY-PARSER AND PUSHING [ADDING] NEW ITEM ON CURRENT LISTS

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extend:true}));
 app.set("view engine", 'ejs');
 
-app.get("/", function(req, res){
-    var today = new Date();
-    var currentday = today.getDay();
-    var day = "";
 
+var items = ["buy food", "cook food", "fat food"];
+
+app.get("/", function(req, res){
+    
     // ##### Chap: 1
+    // var today = new Date();
+    // var currentday = today.getDay();
+    // var day = "";
     // console.log(currentday);
     // if(currentday == 6 || currentday == 0){
     //     res.write("<h1>Yay it's the weekend</h1>");
@@ -21,43 +25,69 @@ app.get("/", function(req, res){
     // }
 
 
-    // ##### Chap: 2
-    // console.log(currentday);
-    // if(currentday == 6 || currentday == 0){
-    //     day = "weekendddd";
-    // }else{
-    //     day = "weekdayyyy";
+    // // ##### Chap: 2
+    // // console.log(currentday);
+    // // if(currentday == 6 || currentday == 0){
+    // //     day = "weekendddd";
+    // // }else{
+    // //     day = "weekdayyyy";
+    // // }
+    // // res.render("list", {kindofday: day});
+
+
+    // // ###### CHAPTER 3
+    // switch(currentday){
+    //     case 0:
+    //     day = "Sunday";
+    //     break;
+    //     case 1:
+    //     day = "Monday";
+    //     break;
+    //     case 2:
+    //     day = "Tuesday";
+    //     break;
+    //     case 3:
+    //     day = "Wedensday";
+    //     break;
+    //     case 4:
+    //     day = "Thursday";
+    //     break;
+    //     case 5:
+    //     day = "Friday";
+    //     break;
+    //     case 6:
+    //     day = "Saturday";
+    //     break;
+    //     default:
+    //         console.log("Error");
     // }
     // res.render("list", {kindofday: day});
 
 
-    // ###### CHAPTER 3
-    switch(currentday){
-        case 0:
-        day = "Sunday";
-        break;
-        case 1:
-        day = "Monday";
-        break;
-        case 2:
-        day = "Tuesday";
-        break;
-        case 3:
-        day = "Wedensday";
-        break;
-        case 4:
-        day = "Thursday";
-        break;
-        case 5:
-        day = "Friday";
-        break;
-        case 6:
-        day = "Saturday";
-        break;
-        default:
-            console.log("Error");
-    }
-    res.render("list", {kindofday: day});
+
+    // # Chapter: 4
+    // PASSING DATA FROM YOUR WEBPAGE TO YOUR SERVER
+    var today = new Date();
+    var options = {
+        weekday:'long',
+        day:'numeric',
+        month:'long',
+    };
+    var currentday = today.toLocaleDateString('en-US', options);
+    res.render("list", {kindofDay: currentday, newlistItems: items});
+
+
+});
+
+// # Chapter: 4
+// POST REQ
+app.post("/", function(req, res){
+    var item = req.body.addNewItem;
+    // console.log(item);
+    // ADD NEW ITEM ON EXCITING ITEMS LISTS
+    items.push(item);
+    // REDIRECTING TO HOME DIR OR PAGE
+    res.redirect('/');
 });
 
 app.listen(3000, function(){
